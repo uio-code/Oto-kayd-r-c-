4bf025f87dead95d3320ba8501ad3ee683dcaba0
uio-code <lancelot007777777@gmail.com>
2026-06-09 08:01:41 +0300

    Initialize settings.gradle.kts for Android project
    Add root configuration for Android project with repositories.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+// Android projesinin kök yapılandırması. Gerekli kütüphane depolarını tanımlar.
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AutoSwiper"
+include(":app")
